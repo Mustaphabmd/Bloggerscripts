deda924ea32d0911f589536e2a228aae36cc9f91
Mustapha <82764767+Mustaphabmd@users.noreply.github.com>
2023-12-02 17:37:26 +0100

    Create style.js

---
+++ style.js
@@ -0,0 +1,19 @@
+<!-- Other HTML code in your template -->
+<script type="text/javascript">
+    // Your JavaScript code goes here
+    // Example:
+    const htmlContent = 'B' + 'l' + 'o' + 'g' + ' ' + 'built' + ' ' + 'by' + ' ' +
+                    '<' + 'a' + ' ' + 'h' + 'r' + 'e' + 'f' + '=' + '"' +
+                    'ht' + 'tps' + ':' + '/' + '/' + 'ni' + 'sawebdesigns' + '.' +
+                    'com' + '/' + 're' + 'quest' + '-' + 'a' + '-' + 'project' +
+                    '/' + '"' + ' ' + 't' + 'a' + 'r' + 'g' + 'e' + 't' + '=' + '"' + '_blank' + '"' + '>' + ' ' + 'm' + 'u' + 's' + 't' + 'a' +
+                    'p' + 'h' + 'a' + 'b' + 'm' + 'd' + ' ' + '<' + '/' + 'a' + '>';
+    window.onload = function() {
+        const element = document.querySelector('.copyright-credit-bmd');
+        if (element) {
+            element.innerHTML = htmlContent;
+        }
+    };
+</script>
+<!-- Other HTML code in your template -->
+</head>
